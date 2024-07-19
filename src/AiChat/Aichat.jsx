61a1ddf4546120559e74f4492799408be45ff948
Gaurav Devrani <gauravdevrani60@gmail.com>
2024-07-19 14:31:58 +0530

    optimized code

--- src/AiChat/Aichat.jsx
+++ src/AiChat/Aichat.jsx
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import React, { useState, useEffect, useRef, lazy, Suspense, useCallback } from 'react';
+import React, { useState, useEffect, useRef, lazy, Suspense } from 'react';
 import { FiTrash2, FiSend } from 'react-icons/fi'; 
 import { HiOutlineClipboardCopy } from 'react-icons/hi'; 
 import { GiMagicLamp } from "react-icons/gi";
-import debounce from 'lodash.debounce';
 
 // Lazy load the ReactMarkdown component
 const ReactMarkdown = lazy(() => import('react-markdown'));
@@ -13,7 +12,8 @@ const Aichat = () => {
   const [answer, setAnswer] = useState('');
   const [generatingAnswer, setGeneratingAnswer] = useState(false);
   const [searchHistory, setSearchHistory] = useState([]);
-  const cache = useRef({}); // Cache for storing API responses
+  const [debounceTimer, setDebounceTimer] = useState(null); // New state for debounce timer
+  const cache = useRef({});
 
   useEffect(() => {
     const storedHistory = localStorage.getItem('searchHistory');
@@ -26,65 +26,67 @@ const Aichat = () => {
     localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
   }, [searchHistory]);
 
-  // Debounced version of the generateAnswer function
-  const debouncedGenerateAnswer = useRef(
-    debounce(async (question) => {
-      setGeneratingAnswer(true);
-      setAnswer('Loading your answer...');
+  const generateAnswer = async (question) => {
+    setGeneratingAnswer(true);
+    setAnswer('Loading your answer...');
 
-      // Check if the answer is in the cache
-      if (cache.current[question]) {
-        setAnswer(cache.current[question]);
-        setGeneratingAnswer(false);
-        return;
-      }
-
-      try {
-        const response = await axios.post(
-          `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${import.meta.env.VITE_API_GENERATIVE_LANGUAGE_CLIENT}`,
-          {
-            contents: [{ parts: [{ text: question }] }],
-          }
-        );
-        const newAnswer = response.data.candidates[0].content.parts[0].text;
-        setAnswer(newAnswer);
-
-        // Cache the new answer
-        cache.current[question] = newAnswer;
-
-        // Update search history
-        if (question.trim() !== '') {
-          setSearchHistory(prevHistory => [...prevHistory, question]);
-        }
-      } catch (error) {
-        console.log(error);
-        setAnswer('Sorry - Something went wrong. Please try again!');
-      }
-
+    if (cache.current[question]) {
+      setAnswer(cache.current[question]);
       setGeneratingAnswer(false);
-    }, 500)
-  ).current;
+      return;
+    }
 
-  // Handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (question.trim() !== '') {
-      debouncedGenerateAnswer(question);
-      setQuestion('');
+    try {
+      const response = await axios.post(
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${import.meta.env.VITE_API_GENERATIVE_LANGUAGE_CLIENT}`,
+        {
+          contents: [{ parts: [{ text: question }] }],
+        }
+      );
+      const newAnswer = response.data.candidates[0].content.parts[0].text;
+      setAnswer(newAnswer);
+      cache.current[question] = newAnswer;
+      if (question.trim() !== '') {
+        setSearchHistory(prevHistory => [...prevHistory, question]);
+      }
+    } catch (error) {
+      console.log(error);
+      setAnswer('Sorry - Something went wrong. Please try again!');
+    }
+
+    setGeneratingAnswer(false);
+  };
+
+  const handleSendClick = () => {
+    if (debounceTimer) {
+      clearTimeout(debounceTimer);
+    }
+    const newDebounceTimer = setTimeout(() => {
+      if (question.trim() !== '') {
+        generateAnswer(question);
+      }
+    }, 1000); // 1 second debounce period
+    setDebounceTimer(newDebounceTimer);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault(); // Prevent the default Enter key behavior (e.g., form submission)
+      handleSendClick();
     }
   };
 
-  const clearHistory = useCallback(() => {
+  const clearHistory = () => {
     setSearchHistory([]);
-  }, []);
+  };
 
-  const removeFromHistory = useCallback((index) => {
+  const removeFromHistory = (index) => {
     setSearchHistory(prevHistory => prevHistory.filter((_, i) => i !== index));
-  }, []);
+  };
 
-  const copyAnswerToClipboard = useCallback(() => {
+  const copyAnswerToClipboard = () => {
     navigator.clipboard.writeText(answer);
-  }, [answer]);
+  };
 
   return (
     <div className="max-w-screen mx-auto bg-[#161315] min-h-screen px-4 py-8">
@@ -92,30 +94,31 @@ const Aichat = () => {
         <GiMagicLamp className="mr-2 text-[#F58840] text-4xl" /> IntelliGenie
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {/* Chat Section */}
         <div className="col-span-2 bg-[#161315] rounded-lg p-4">
+          {/* Suspense component to handle the loading state */}
           <Suspense fallback={<div className="text-gray-200 md:text-lg mb-4">Loading...</div>}>
+            {/* ReactMarkdown component is rendered only when it's needed */}
             <ReactMarkdown className="text-gray-200 md:text-lg mb-4">{answer}</ReactMarkdown>
           </Suspense>
-          <form onSubmit={handleSubmit} className="mb-4 flex items-center">
+          <div className="mb-4 flex items-center">
             <input
               required
               type="text"
               className="border border-gray-300 rounded-full w-full my-2 p-3 mr-2 focus:border-[#F58840] focus:outline-none"
               value={question}
               onChange={(e) => setQuestion(e.target.value)}
+              onKeyDown={handleKeyDown} // Add onKeyDown event handler
               placeholder="Ask anything..."
             />
             <button
-              type="submit"
-              className={`bg-[#F58840] text-gray-200 p-3 rounded-full hover:bg-gray-600 transition-all duration-300 ${
-                generatingAnswer ? 'opacity-50 cursor-not-allowed' : ''
-              }`}
+              type="button"
+              onClick={handleSendClick}
+              className={`bg-[#F58840] text-gray-200 p-3 rounded-full hover:bg-gray-600 transition-all duration-300 ${generatingAnswer ? 'opacity-50 cursor-not-allowed' : ''}`}
               disabled={generatingAnswer}
             >
               <FiSend className="inline-block mr-1 text-xl" />
             </button>
-          </form>
+          </div>
           {answer && (
             <button
               onClick={copyAnswerToClipboard}
@@ -125,7 +128,6 @@ const Aichat = () => {
             </button>
           )}
         </div>
-        {/* Search History Section */}
         <div className="col-span-1 bg-[#1a151a] min-h-auto border-l border-gray-300 p-4">
           <div className="flex justify-between items-center mb-4">
             <h2 className="text-lg font-semibold text-[#F58840]">Search History</h2>
@@ -157,4 +159,4 @@ const Aichat = () => {
   );
 };
 
-export default React.memo(Aichat);
+export default Aichat;
